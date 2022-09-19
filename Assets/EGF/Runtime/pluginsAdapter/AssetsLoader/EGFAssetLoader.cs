d24b2daa51c34ae733055bbd2b868ca25630afed
Eric <eric01cbymikufan@outlook.com>
2022-09-19 23:11:21 +0800

    新版异步加载接口

---
+++ Assets/EGF/Runtime/pluginsAdapter/AssetsLoader/EGFAssetLoader.cs
@@ -0,0 +1,162 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using AddressablesMaster;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceProviders;
+using UnityEngine.SceneManagement;
+using Object = UnityEngine.Object;
+
+namespace EGF.Runtime
+{
+    // TODO：给加载资源操作添加取消
+    public class EGFAssetLoader : MonoSingleton<EGFAssetLoader>,IAssetLoader
+    {
+        protected override void Initialization()
+        {
+            ManageAddressables.Initialize();
+            EgfEntry.RegisterModule<IAssetLoader>(this);
+        }
+
+        protected override void Release()
+        {
+            EgfEntry.UnRegisterModule<IAssetLoader>();
+        }
+        
+        // ----------------------------------------------------------------------------------------------------
+
+        public T Load<T>(string path) where T : Object
+        {
+            return ManageAddressables.LoadAssetSync<T>(path);
+        }
+
+        public T Load<T>(AssetReferenceT<T> reference) where T : Object
+        {
+            return ManageAddressables.LoadAssetSync<T>(reference);
+        }
+
+        public async UniTask<T> LoadAsync<T>(string path) where T : Object
+        {
+            var task = ManageAddressables.LoadAssetAsync<T>(path).AsUniTask();
+            var result = await task;
+
+            return result.Value;
+        }
+
+        public async UniTask<T> LoadAsync<T>(AssetReferenceT<T> reference) where T : Object
+        {
+            var task = ManageAddressables.LoadAssetAsync(reference).AsUniTask();
+            var result = await task;
+            
+            return result.Value;
+        }
+
+        // ----------------------------------------------------------------------------------------------------
+        
+        public GameObject Instantiate(string path)
+        {
+            return ManageAddressables.InstantiateSyncWithAutoRelease(path);
+        }
+
+        public GameObject Instantiate(AssetReferenceT<GameObject> reference)
+        {
+            return ManageAddressables.InstantiateSyncWithAutoRelease(reference);
+        }
+
+        public async UniTask<GameObject> InstantiateAsync(string path)
+        {
+            var task = ManageAddressables.InstantiateAsyncWithAutoRelease(path).AsUniTask();
+            var result = await task;
+
+            return result;
+        }
+
+        public async UniTask<GameObject> InstantiateAsync(AssetReferenceT<GameObject> reference)
+        {
+            var task = ManageAddressables.InstantiateAsyncWithAutoRelease(reference).AsUniTask();
+            var result = await task;
+
+            return result;
+        }
+        
+        // ----------------------------------------------------------------------------------------------------
+
+        public async UniTask<SceneInstance> AddScene(string scenePath, bool activateOnLoad = true)
+        {
+            var task = ManageAddressables.LoadSceneAsync(scenePath, LoadSceneMode.Additive, activateOnLoad).AsUniTask();
+            var result = await task;
+
+            return result.Value;
+        }
+
+        public async UniTask<SceneInstance> AddScene(AssetReference sceneReference, bool activateOnLoad = true)
+        {
+            var task = ManageAddressables.LoadSceneAsync(sceneReference, LoadSceneMode.Additive, activateOnLoad).AsUniTask();
+            var result = await task;
+
+            return result.Value;
+        }
+
+        public UniTask RemoveScene(string scenePath)
+        {
+            return ManageAddressables.UnloadSceneAsync(scenePath).AsUniTask();
+        }
+        
+        public UniTask RemoveScene(AssetReference sceneReference)
+        {
+            return ManageAddressables.UnloadSceneAsync(sceneReference).AsUniTask();
+        }
+
+        public async UniTask<SceneInstance> Switch2Scene(string scenePath, Action<float> progressHandler, bool activateOnLoad = true)
+        {
+            // 加载场景
+            var operate = Addressables.LoadSceneAsync(scenePath, LoadSceneMode.Single, activateOnLoad);
+            
+            // 开始跟踪进度条
+            var cts = new CancellationTokenSource();
+            SceneLoadProgress(operate, progressHandler,cts.Token).Forget();
+            
+            // 等待加载完成
+            var result = await operate.Task.AsUniTask();;
+            
+            // 停止跟踪进度条
+            cts.Cancel();
+            cts.Dispose();
+            
+            return result;
+        }
+
+        public async UniTask<SceneInstance> Switch2Scene(AssetReference sceneReference, Action<float> progressHandler, bool activateOnLoad = true)
+        {
+            // 加载场景
+            var operate = Addressables.LoadSceneAsync(sceneReference, LoadSceneMode.Single, activateOnLoad);
+            
+            // 开始跟踪进度条
+            var cts = new CancellationTokenSource();
+            SceneLoadProgress(operate, progressHandler,cts.Token).Forget();
+            
+            // 等待加载完成
+            var result = await operate.Task.AsUniTask();;
+            
+            // 停止跟踪进度条
+            cts.Cancel();
+            cts.Dispose();
+            
+            return result;
+        }
+
+        async UniTask SceneLoadProgress(AsyncOperationHandle<SceneInstance> operationHandle, Action<float> progressHandler, CancellationToken cancellationToken)
+        {
+            while (operationHandle.PercentComplete < 1 && !cancellationToken.IsCancellationRequested)
+            {
+                progressHandler(operationHandle.PercentComplete);
+                await UniTask.Yield();
+            }
+            progressHandler(1);
+        }
+    }
+}
---
+++ Assets/EGF/Runtime/pluginsAdapter/AssetsLoader/IAssetLoader.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.ResourceProviders;
+using Object = UnityEngine.Object;
+
+namespace EGF.Runtime
+{
+    public interface IAssetLoader
+    {
+        // ----------------------------------------------------------------------------------------------------
+        // 载入资源或游戏对象
+        T Load<T>(string path) where T : Object;
+        T Load<T>(AssetReferenceT<T> reference) where T : Object;
+        UniTask<T> LoadAsync<T>(string path) where T : Object;
+        UniTask<T> LoadAsync<T>(AssetReferenceT<T> reference) where T : Object;
+        
+        // ----------------------------------------------------------------------------------------------------
+        // 直接载入并实例化游戏对象
+        GameObject Instantiate(string path);
+        GameObject Instantiate(AssetReferenceT<GameObject> reference);
+        UniTask<GameObject> InstantiateAsync(string path);
+        UniTask<GameObject> InstantiateAsync(AssetReferenceT<GameObject> reference);
+        
+        // ----------------------------------------------------------------------------------------------------
+        // 添加、移出、切换场景
+        
+        UniTask<SceneInstance> AddScene(string scenePath, bool activateOnLoad = true);
+        UniTask<SceneInstance> AddScene(AssetReference sceneReference, bool activateOnLoad = true);
+        
+        UniTask RemoveScene(string scenePath);
+        UniTask RemoveScene(AssetReference sceneReference);
+        
+        UniTask<SceneInstance> Switch2Scene(string scenePath, Action<float> progressHandler, bool activateOnLoad = true);
+        UniTask<SceneInstance> Switch2Scene(AssetReference sceneReference, Action<float> progressHandler, bool activateOnLoad = true);
+    }
+}
