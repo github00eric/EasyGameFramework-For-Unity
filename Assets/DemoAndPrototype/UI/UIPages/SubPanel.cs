2a038123b893ba5088e2625adc63408b037bfe5d
Eric <eric01cbymikufan@outlook.com>
2022-09-22 18:05:17 +0800

    UI 完善Demo展示

--- Assets/DemoAndPrototype/UI/UIPages/SubPanel.cs
+++ Assets/DemoAndPrototype/UI/UIPages/SubPanel.cs
@@ -1,10 +1,9 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using EGF.Runtime;
 using UnityEngine;
 
-public class StartPanel : UiView
+public class SubPanel : UiView
 {
     
 }
